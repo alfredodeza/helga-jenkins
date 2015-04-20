1f5c58e93eccccf150906149f08ea27834d9e052
Alfredo Deza <alfredo@deza.pe>
2015-04-19 20:45:40 -0400

    actually talk to jenkins

--- helga_jenkins.py
+++ helga_jenkins.py
@@ -1,13 +1,33 @@
-import os
-import socket
 from helga.plugins import command
 from helga import log, settings
+import jenkins
 
 logger = log.getLogger(__name__)
+
+
+def status(conn, name):
+    return conn.get_job_info(name)
+
+
+def jobs(conn, *args):
+    return conn.get_jobs()
 
 
 @command('jenkins', aliases=['ci'], help='control jenkins', priority=0)
 def helga_jenkins(client, channel, nick, message, cmd, args):
     username = getattr(settings, 'JENKINS_USERNAME')
     password = getattr(settings, 'JENKINS_PASSWORD')
-    pass
+    url = getattr(settings, 'JENKINS_URL')
+    if not url:
+        return 'no JENKINS_URL is configured, cannot continue'
+
+    sub_commands = {
+        'status': status,
+        'jobs': jobs,
+    }
+
+    sub_command = args[0]
+
+    conn = jenkins.Jenkins(url, username=username, password=password)
+
+    return sub_commands[sub_command](conn, *args)
