163585443165c44dcfef9b9349309beb5b50779d
Alfredo Deza <alfredo@deza.pe>
2015-04-22 10:32:45 -0400

    add support for multi-user token auth
    Signed-off-by: Alfredo Deza <alfredo@deza.pe>

--- helga_jenkins.py
+++ helga_jenkins.py
@@ -22,6 +22,10 @@ def health(conn, *args):
 def builds(conn, *args):
     args = list(args)
     args.pop(0)  # get rid of the command
+
+    # FIXME: need to call assert_job_exists()
+    # to get some validation done here instead of assuming
+    # input is right
     name = args.pop(0)  # get rid of the name
     info = conn.get_job_info(name)
     sub_commands = {
@@ -56,13 +60,60 @@ def builds(conn, *args):
     ]
 
 
-@command('jenkins', aliases=['ci'], help='control jenkins', priority=0)
-def helga_jenkins(client, channel, nick, message, cmd, args):
-    username = getattr(settings, 'JENKINS_USERNAME', None)
-    password = getattr(settings, 'JENKINS_PASSWORD', None)
+def parse_args(args):
+    """
+    Translate args that may look like::
+
+        ["command", "sub-command"]
+
+    """
+
+def build(conn, *args):
+    args = list(args)
+    args.pop(0)  # get rid of the command
+
+    # FIXME: need to call assert_job_exists()
+    # to get some validation done here instead of assuming
+    # input is right
+    name = args.pop(0)  # get rid of the name
+    pass
+
+
+def connect(nick):
+    """
+    Since a user can have simple authentication with a single user/password or
+    define a set of IRC nick to Jenkin's users with API tokens, this helper
+    untangles this and returns a connection.
+
+    If no authentication is configured, just a connection is returned with no
+    authentication (probably read-only, depending on Jenkins settings)
+    """
     url = getattr(settings, 'JENKINS_URL')
     if not url:
-        return 'no JENKINS_URL is configured, cannot continue'
+        raise RuntimeError('no JENKINS_URL is configured, cannot continue')
+    username = getattr(settings, 'JENKINS_USERNAME', None)
+    password = getattr(settings, 'JENKINS_PASSWORD', None)
+    credentials = getattr(settings, 'JENKINS_CREDENTIALS', {})
+    user_auth = credentials.get(nick)
+
+    # favor user creds first, fallback to simple creds, and ultimately
+    # fallback to None which is allowed
+    user = user_auth.get('username', username)
+    pass_ = user_auth.get('password', username)
+
+    return jenkins.Jenkins(
+        url,
+        username=user,
+        password=_pass,
+    )
+
+
+@command('jenkins', aliases=['ci'], help='control jenkins', priority=0)
+def helga_jenkins(client, channel, nick, message, cmd, args):
+    try:
+        conn = connect(nick)
+    except RuntimeError as error:
+        return str(error)
 
     sub_commands = {
         # XXX commented out because they need trimming
@@ -70,10 +121,8 @@ def helga_jenkins(client, channel, nick, message, cmd, args):
         #'jobs': jobs,
         #'health': health,
         'builds': builds,
+        'build': build,
     }
 
     sub_command = args[0]
-
-    conn = jenkins.Jenkins(url, username=username, password=password)
-
     return sub_commands[sub_command](conn, *args)
